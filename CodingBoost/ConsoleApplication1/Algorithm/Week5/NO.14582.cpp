a3ca992beb591229dd9b801999b129e974dbe595
Soohan Lee <lubsuhan09@naver.com>
2022-08-03 11:13:55 +0900

    5주차 과제

---
+++ CodingBoost/ConsoleApplication1/Algorithm/Week5/NO.14582.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int jeminis[9], startlink[9];
+    int state = 0;
+    int JScore = 0;
+    int SScore=0;
+    for (int i = 0; i <9; i++) cin >> jeminis[i]; //jeminis 점수 회차별로 입력받기
+    for (int i = 0; i < 9; i++) cin >> startlink[i];//starlink 점수 회차별로 입력받기
+    for (int i = 0; i < 9; i++) {
+        JScore += jeminis[i]; //jeminis 점수 회차별로 합치기
+        if (JScore > SScore) state = 1; //jeminis가 이기고 있는 상태
+        SScore += startlink[i]; //statlink 점수 회차별로 합치기
+        if (JScore < SScore && state == 1) state = 2; //starlink가 jeminis를 역전한 상태
+    }
+    if (state == 2) cout << "Yes"<<endl;
+    else cout << "No"<<endl;
+}
